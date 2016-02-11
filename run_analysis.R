503b0ead50786818ee45d3fda9203b3c08d62b2c
Thomas Martin <tmartin@live.fr>
2016-02-11 12:44:17 +0100

    Cleaning, comments & renaming of variables. Adding write to txt file for averageDataSet

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,17 @@
 ## 
 ## run_analysis.R
 ##
-## This Script execute the
+## This Script execute the following operations :
+## 0. Loading data from different files.
+## 1. Merge training data & test data for different data elements.
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+## 3. Uses descriptive activity names to name the activities in the data set
+## 4. Appropriately labels the data set with descriptive variable names.
+## 5. Creates a second, independent tidy data set from 4 with the average 
+##    of each variable for each activity and each subject. Save the result in averageDataSet.txt
 ## 
 ## Author   - Thomas MARTIN
-## Date     - 09/02/2016
+## Date     - 11/02/2016
 ## Email    - tmartin@live.fr
 ## Version  - 1.0
 ##
@@ -20,57 +27,80 @@ require(dplyr)
 library(dplyr)
 
 ## 
-## Data Loading
+## 0.Data Loading
 ##
 
-featuresLabel <- read.table("features.txt", stringsAsFactors = FALSE)
-xTrain <- read.table("train/X_train.txt", stringsAsFactors = FALSE)
-xTest <- read.table("test/X_test.txt", stringsAsFactors = FALSE)
-yTrain <- read.table("train/y_train.txt", stringsAsFactors = FALSE)
-yTest <- read.table("test/y_test.txt", stringsAsFactors = FALSE)
-activitiesLabel <- read.table("activity_labels.txt", stringsAsFactors = FALSE)
-subjectTrain <- read.table("train/subject_train.txt", stringsAsFactors = FALSE)
+## Names of columns's Data and labels for activities.
+originDataColomnsNames <- read.table("features.txt", stringsAsFactors = FALSE)
+activitiesLabels <- read.table("activity_labels.txt", stringsAsFactors = FALSE)
+
+## Data in original format split in two data.frames.
+originDataTrain <- read.table("train/X_train.txt", stringsAsFactors = FALSE)
+originDataTest <- read.table("test/X_test.txt", stringsAsFactors = FALSE)
+
+## Activities linked to Data in original format. Split in two data.frames too.
+activitiesTrain <- read.table("train/y_train.txt", stringsAsFactors = FALSE)
+activitiesTest <- read.table("test/y_test.txt", stringsAsFactors = FALSE)
+
+## Subjects linked to Data in original format. Split in two data.frames too. 
 subjectTest <- read.table("test/subject_test.txt", stringsAsFactors = FALSE)
+subjectTrain <- read.table("train/subject_train.txt", stringsAsFactors = FALSE)
+
 
 ##
 ##  1.Merges the training and the test sets to create one data set.
 ##
 
-xTotal <- rbind(xTrain, xTest)
-rm(xTrain, xTest)
-
-yTotal <- rbind(yTrain, yTest)
-rm(yTrain, yTest)
-
+originDataTotal <- rbind(originDataTrain, originDataTest)
+activitiesTotal <- rbind(activitiesTrain, activitiesTest)
 subjectTotal <- rbind(subjectTrain, subjectTest)
-rm(subjectTest, subjectTrain)
+
+## remove not used anymore variables.
+rm(originDataTrain, originDataTest) 
+rm(subjectTest, subjectTrain) 
+rm(activitiesTrain, activitiesTest) 
 
 ##
 ##  2.Extracts only the measurements on the mean and standard deviation for each measurement.
 ##
 
-usefulColumnsId <- featuresLabel$V1[grepl('mean\\(\\)|std\\(\\)', featuresLabel$V2)]
-xTotal <- select(xTotal, usefulColumns)
+usefulColumnsId <- originDataColomnsNames$V1[grepl('mean\\(\\)|std\\(\\)', originDataColomnsNames$V2)]
+extractDataTotal <- select(originDataTotal, usefulColumnsId)
+
+## remove not used anymore variables.
+rm(originDataTotal) 
+
 
 ##
 ##  3.Uses descriptive activity names to name the activities in the data set
 ##
-activities <- activitiesLabel$V2[yTotal$V1]
+
+labelledActivitiesTotal <- activitiesLabels$V2[activitiesTotal$V1]
+
+## remove not used anymore variables.
+rm(activitiesTotal, activitiesLabels) 
 
 
 ##
 ##  4.Appropriately labels the data set with descriptive variable names.
 ## 
-usefulColumnsName <- featuresLabel$V2[usefulColumnsId]
-colnames(xTotal) <- usefulColumnsName
-xTotal <- cbind(xTotal, "subject" = subjectTotal$V1)
-finalDataSet <- cbind(xTotal, "activity" = activities)
-rm(xTotal)
+
+usefulColumnsName <- originDataColomnsNames$V2[usefulColumnsId]
+colnames(extractDataTotal) <- usefulColumnsName
+extractDataTotal <- cbind(extractDataTotal, "subject" = subjectTotal$V1)
+finalDataSet <- cbind(extractDataTotal, "activity" = labelledActivitiesTotal)
+
+## remove not used anymore variables.
+rm(extractDataTotal, labelledActivitiesTotal, subjectTotal)
+rm(originDataColomnsNames, usefulColumnsId, usefulColumnsName)
+
 
 ##
 ##  5.From the data set in step 4, creates a second, independent tidy data set with the average 
-##    of each variable for each activity and each subject.
+##    of each variable for each activity and each subject. Save the result in averageDataSet.txt
 ##
 averageDataSet <- finalDataSet %>%
   group_by(activity, subject) %>%
   summarise_each(funs(mean))
+
+write.table(averageDataSet, "averageDataSet.txt", row.names = FALSE)
